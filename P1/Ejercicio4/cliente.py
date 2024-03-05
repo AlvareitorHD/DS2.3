777d12995d9c7e43a093e5ea0767a5d571110165
lu1smgb <lu1smgb.gacc@gmail.com>
2024-03-05 19:22:15 +0100

    Interfaz de prueba

--- P1/Ejercicio4/cliente.py
+++ P1/Ejercicio4/cliente.py
@@ -13,4 +13,76 @@ class Cliente:
         return self.gestor.solicitar(ctx, verbose)
     
     def iniciar_iu(self):
-        pass
+        # Nodo padre de elementos
+        root = tk.Tk()
+        
+        # Titulo de la ventana
+        root.title("Simulador - Ejercicio 4 - DS (2.3)")
+        
+        # Frame que contendra la informacion del salpicadero
+        # Propiedades del frame
+        salpicadero_frame = tk.Frame(
+            master=root,
+            border='2px',
+            relief='ridge'
+        )
+        # Propiedades dentro del layout
+        # Pack sera el tipo de layout para los frames del salpicadero y los mandos
+        salpicadero_frame.pack(
+            expand=True,
+            fill='both',
+            side='top',
+            anchor='n',
+            padx=10,
+            pady=10
+        )
+        
+        # Etiqueta dentro del frame del salpicadero
+        label_salpicadero_frame = tk.Label(
+            master=salpicadero_frame,
+            text='Salpicadero',
+            font=('Roboto', 12),
+            foreground='#553333',
+            background='#aaffff'
+        )
+        # El frame tambien tiene su propio layout
+        label_salpicadero_frame.pack(
+            expand=True,
+            fill='x',
+            side='top',
+            anchor='n',
+            ipadx=5,
+            ipady=5
+        )
+        
+        # Repetimos lo mismo para los mandos
+        mandos_frame = tk.Frame(
+            master=root,
+            border='2px',
+            relief='ridge'
+        )
+        mandos_frame.pack(
+            expand=True,
+            fill='both',
+            side='top',
+            padx=10,
+            pady=10
+        )
+        label_mandos_frame = tk.Label(
+            master=mandos_frame,
+            text='Mandos',
+            font=('Roboto', 12),
+            foreground='#553333',
+            background='#aaffff'
+        )
+        label_mandos_frame.pack(
+            expand=True,
+            fill='x',
+            side='top',
+            anchor='n',
+            ipadx=5,
+            ipady=5
+        )
+        
+        # Abrimos la ventana
+        root.mainloop()
