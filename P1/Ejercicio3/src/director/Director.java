e2c8e6b4b4c99c00885210e715879f8da2c63abe
migue-maca-IngInfo <migue.inginfo@gmail.com>
2024-03-13 22:22:19 +0100

    Implementación provisional del director

--- P1/Ejercicio3/src/director/Director.java
+++ P1/Ejercicio3/src/director/Director.java
@@ -1,16 +1,35 @@
 package director;
 
 import constructor.Constructor;
+import constructor.ConstructorBicicletaCarretera;
+import constructor.ConstructorBicicletaMontana;
 
 public class Director {
 
     public Director() { ; }
 
     public void hacerBicicletaMontana(Constructor constructor) {
-
+        if (constructor instanceof ConstructorBicicletaMontana) {
+            constructor.recomponer();
+            constructor.construirManillar("RECTO");
+            ((ConstructorBicicletaMontana) constructor).construirSuspension("ROSCADA", 1);
+            constructor.construirFrenos("DISCO", 2);
+            constructor.construirTransmision("SHIMANO");
+            constructor.construirCuadro("ALUMINIO");
+            constructor.construirSillin("SEMIRREDONDEADO");
+            constructor.construirRuedas("OFF-ROAD", 2);
+        }
     }
 
     public void hacerBicicletaCarretera(Constructor constructor) {
-
+        if (constructor instanceof ConstructorBicicletaCarretera) {
+            constructor.recomponer();
+            constructor.construirManillar("ERGONÓMICO");
+            constructor.construirFrenos("DISCO", 2);
+            constructor.construirTransmision("SHIMANO");
+            constructor.construirCuadro("FIBRA CARBONO");
+            constructor.construirSillin("ESTRECHO");
+            constructor.construirRuedas("ESCALADORAS", 2);
+        }
     }
 }
