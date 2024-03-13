1e2537ed439fd7d5d09e076a2250610cc513406a
migue-maca-IngInfo <migue.inginfo@gmail.com>
2024-03-13 22:44:07 +0100

    Implementación provisional del programa (main)

--- P1/Ejercicio3/src/PruebaEjercicio3.java
+++ P1/Ejercicio3/src/PruebaEjercicio3.java
@@ -1,7 +1,22 @@
+import bicicleta.Bicicleta;
+import constructor.ConstructorBicicletaMontana;
+import decorador.DecoradorBicicletaConEstampado;
+import decorador.DecoradorBicicletaConFunda;
+import director.Director;
+
 public class PruebaEjercicio3 {
 
     public static void main(String[] args) {
-        System.out.println("\nHello world\n");
+        // Parte Builder:
+        Director director = new Director();
+        ConstructorBicicletaMontana constructorBiciMontana = new ConstructorBicicletaMontana();
+        director.hacerBicicletaMontana(constructorBiciMontana);
+        Bicicleta biciMontana = constructorBiciMontana.obtenerResultado();
+
+        // Parte Decorator:
+        Bicicleta biciMontanaConEstampado = new DecoradorBicicletaConEstampado(biciMontana);
+        Bicicleta biciMontanaConEstampadoYFunda = new DecoradorBicicletaConFunda(biciMontanaConEstampado);
+        System.out.println(biciMontanaConEstampadoYFunda);
     }
 
 }
