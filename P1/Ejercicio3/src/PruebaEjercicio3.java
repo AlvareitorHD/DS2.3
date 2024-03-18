5f04c175d20d9a885c6557fc7687ec9f641bcd3f
migue-maca-IngInfo <migue.inginfo@gmail.com>
2024-03-18 17:52:59 +0100

    Modificado programa principal

--- P1/Ejercicio3/src/PruebaEjercicio3.java
+++ P1/Ejercicio3/src/PruebaEjercicio3.java
@@ -10,31 +10,55 @@ public class PruebaEjercicio3 {
         // Crear el director para manejar las construcciones:
         Director director = new Director();
 
-        // Construir una bicicleta de montaña decorada (con estampado y funda):
+
+        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+        // ++ Construir una bicicleta de montaña sin más:                                                          ++ //
+        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
         // Primero, se crea el constructor de la bicicleta de montaña:
         ConstructorBicicletaMontana constructorBiciMontana = new ConstructorBicicletaMontana();
-        // Segundo, se manda a construir la bicicleta de montaña:
+        // Segundo, se manda a construir la bicicleta de montaña a través del director:
         director.hacerBicicletaMontana(constructorBiciMontana);
-        // Tercero, se crea el constructor para decorar la bicicleta de montaña que se ha construido:
+        // Tercero, se obtiene la bicicleta de montaña a través de su constructor:
+        Bicicleta biciMontana = constructorBiciMontana.obtenerResultado();
+        // Finalmente, se imprime la bicicleta:
+        System.out.println("\nBicicleta de montaña normal:\n" + biciMontana);
+
+
+        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+        // ++ Decorar la bicicleta de montaña anterior (con estampado y funda):                                    ++ //
+        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+        // Primero, se crea el constructor para decorar la bicicleta de montaña que se ha construido previamente:
         ConstructorBicicletaDecorada constructorBiciDecorada =
-                new ConstructorBicicletaDecorada(constructorBiciMontana.obtenerResultado());
-        // Cuarto, se manda a decorar la bicicleta de montaña:
+                new ConstructorBicicletaDecorada(biciMontana);
+        // Segundo, se manda a decorar la bicicleta de montaña:
         director.hacerBicicletaDecorada(constructorBiciDecorada);
-        // Quinto, se obtiene la bicicleta de montaña con la decoración añadida:
+        // Tercero, se obtiene la bicicleta de montaña con la decoración añadida:
         Bicicleta biciMontanaDecorada = constructorBiciDecorada.obtenerResultado();
         // Finalmente, se imprime la bicicleta:
-        System.out.println(biciMontanaDecorada);
+        System.out.println("\nBicicleta de montaña anterior decorada (con estampado y funda):\n" +
+                biciMontanaDecorada);
 
 
+        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+        // ++ Para construir una bicicleta de carretera decorada (con estampado y funda):                          ++ //
+        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
 
-        // Para construir una bicicleta de carretera decorada (con estampado y funda), los pasos son los mismos:
+        // Primero, se crea el constructor de la bicicleta de carretera:
         ConstructorBicicletaCarretera constructorBicicletaCarretera = new ConstructorBicicletaCarretera();
+        // Segundo, se manda a construir la bicicleta de carretera a través del director:
         director.hacerBicicletaCarretera(constructorBicicletaCarretera);
+        // Tercero, se crea el constructor para decorar la bicicleta de carretera que se ha construido previamente:
         ConstructorBicicletaDecorada constructorBiciDecorada2 =
                 new ConstructorBicicletaDecorada(constructorBicicletaCarretera.obtenerResultado());
+        // Cuarto, se manda a decorar la bicicleta de carretera:
         director.hacerBicicletaDecorada(constructorBiciDecorada2);
+        // Quinto, se obtiene la bicicleta de carretera con la decoración añadida:
         Bicicleta biciCarreteraDecorada = constructorBiciDecorada2.obtenerResultado();
-        System.out.println(biciCarreteraDecorada);
+        // Finalmente, se imprime la bicicleta:
+        System.out.println("\nBicicleta de carretera decorada (con estampado y funda):\n" +
+                biciCarreteraDecorada);
     }
 
 }
