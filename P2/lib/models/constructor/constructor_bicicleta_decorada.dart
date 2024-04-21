f50395a17556c41cf84441e53cc7f369ad939289
Ginex5 <e.ginestorresa@go.ugr.es>
2024-04-21 17:45:58 +0200

    Modificado función asociar imagen en constructor decorada

--- P2/lib/models/constructor/constructor_bicicleta_decorada.dart
+++ P2/lib/models/constructor/constructor_bicicleta_decorada.dart
@@ -40,21 +40,16 @@ class ConstructorBicicletaDecorada extends Constructor {
   void asociarImagenBicicleta() {
     // Verifica primero si la bicicleta es de montaña o carretera.
     bool esMontana = bicicleta?.tipoBicicleta == "montana";
-    // Intenta hacer cast de la bicicleta a DecoradorBicicletaConFunda y verifica si tiene funda.
-    if (bicicleta?.imagenRepresentativa == BICI_MON_FUN ||
-        bicicleta?.imagenRepresentativa == BICI_CAR_FUN) {
-      bicicleta?.asociarImagen(esMontana ? BICI_MON_FUN : BICI_CAR_FUN);
-    }
+
     // Si no tiene funda, verifica si tiene estampado.
-    else if ((bicicleta is DecoradorBicicletaConEstampado)) {
+    if ((bicicleta is DecoradorBicicletaConEstampado)) {
       bicicleta?.asociarImagen(esMontana ? BICI_MON_EST : BICI_CAR_EST);
     } // Si no tiene funda, verifica si tiene estampado.
     else if ((bicicleta is DecoradorBicicletaConFunda)) {
       bicicleta?.asociarImagen(esMontana ? BICI_MON_FUN : BICI_CAR_FUN);
-    }
-    // Si no tiene ni funda ni estampado, asigna la imagen por defecto según el tipo de bicicleta.
-    else {
+    } else {
       bicicleta?.asociarImagen(esMontana ? BICI_MON : BICI_CAR);
+    
     }
   }
 
