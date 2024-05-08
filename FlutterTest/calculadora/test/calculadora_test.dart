0c6546a5c93d09bc5b1b7eab00d943131ba63874
lu1smgb <lu1smgb.gacc@gmail.com>
2024-05-08 15:53:30 +0200

    Agregados test restantes de calculadora

--- FlutterTest/calculadora/test/calculadora_test.dart
+++ FlutterTest/calculadora/test/calculadora_test.dart
@@ -10,6 +10,38 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:test_app_1/operations.dart';
 
 void main() {
+
+  test('Los valores cambian correctamente', () {
+
+    Random rng = Random();
+    double num1 = rng.nextDouble() * 1000000;
+    double num2 = rng.nextDouble() * 1000000;
+
+    Operacion op = Suma(num1, num2);
+
+    op.num1 = rng.nextDouble() * 1000000;
+    op.num2 = rng.nextDouble() * 1000000;
+
+    expect(op.calcular(), op.num1 + op.num2);
+
+  });
+
+  test('El tipo de operacion cambia correctamente', () {
+
+    Random rng = Random();
+    double num1 = rng.nextDouble() * 1000000;
+    double num2 = rng.nextDouble() * 1000000;
+
+    Operacion op = Suma(num1, num2);
+    expect(op.num1, num1);
+    expect(op.num2, num2);
+
+    op = Resta(num1, num2);
+    expect(op.num1, num1);
+    expect(op.num2, num2);
+
+  });
+
   test('Las operaciones dan el resultado esperado', () {
 
     Random rng = Random();
