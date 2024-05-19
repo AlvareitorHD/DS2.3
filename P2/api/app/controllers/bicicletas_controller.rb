a5ba368e43f32502dbaaf9dcd37c3e8a5a62fa49
Ginex5 <e.ginestorresa@go.ugr.es>
2024-05-19 18:53:31 +0200

    Descripción de los cambios que has realizado

--- P2/api/app/controllers/bicicletas_controller.rb
+++ P2/api/app/controllers/bicicletas_controller.rb
@@ -1,3 +1,5 @@
+# Maneja las solicitudes HTTP enviadas desde la aplicación Flutter y realiza operaciones en la base de datos.
+
 class BicicletasController < ApplicationController
 
     def create
@@ -9,16 +11,22 @@ class BicicletasController < ApplicationController
         when 'carretera'
             @bicicleta = Bicicleta.new(bicicleta_carretera_params)
         end
-
+        # nil es sinonimo de null
+        # con save, intenta guardar en la base de datos
         if @bicicleta != nil && @bicicleta.save
-            render json: @tarea, status: :created
+            render json: @bicicleta, status: :created
         else
-            render json: @tarea.errors, status: :unprocessable_entity
+            render json: @bicicleta.errors, status: :unprocessable_entity
         end
     end
 
     def update
-        # ...
+        @bicicleta = Bicicleta.find(params[:id])
+        if @bicicleta.update(bicicleta_params)
+            render json: @bicicleta, status: :ok
+        else
+            render json: @bicicleta.errors, status: :unprocessable_entity
+        end
     end
 
     def index
@@ -29,7 +37,9 @@ class BicicletasController < ApplicationController
         # ...
     end
 
-    def bicicleta_carretera_params
+
+
+    def bicicleta_params
         params.require(:bicicleta).permit(:tipo_manillar, 
                                           :tipo_frenos,
                                           :num_frenos,
@@ -40,11 +50,19 @@ class BicicletasController < ApplicationController
                                           :num_ruedas,
                                           :imagen_representativa,
                                           :tipo_bicicleta)
-    end
-
-    def bicicleta_montana_params
-        bicicleta_carretera_params.permit(:tipo_suspension,
-                                          :num_suspensiones)
-    end
+      end
+    
+      def bicicleta_carretera_params
+        bicicleta_params
+      end
+    
+      def bicicleta_montana_params
+        bicicleta_params.merge(
+          params.require(:bicicleta).permit(
+            :tipo_suspension,
+            :num_suspensiones
+          )
+        )
+      end
 
 end
--- P2/lib/controlador_backend.dart
+++ P2/lib/controlador_backend.dart
@@ -3,27 +3,40 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ControladorBackend {
+  final String apiUrl = "http://localhost:3000/bicicletas";
+  final http.Client client;
 
-  List<Bicicleta> listaBicicletas = [];
-  final String apiUrl = "https://localhost:3000/bicicletas";
+  ControladorBackend({http.Client? client}) : client = client ?? http.Client();
 
-  ControladorBackend(this.listaBicicletas);
-
+  // Solicitud POST
   Future<void> crearBicicleta(Bicicleta bicicleta) async {
+    //final response = await http.post(Uri.parse(apiUrl),
+    final response = await client.post(Uri.parse(apiUrl),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: jsonEncode(bicicleta
+            .toJson()) // Duda: si es bicicleta montana, hace llamada a BicicletaMontana.toJson() ???
+        );
 
-    final response = await http.post(
-      Uri.parse(apiUrl),
-      headers: <String, String> {
-        'Content-Type': 'application/json; charset=UTF-8'
-      },
-      body: jsonEncode(bicicleta.toJson()) // Duda: si es bicicleta montana, hace llamada a BicicletaMontana.toJson() ???
-    );
-    if (response.statusCode == 201) {
-      listaBicicletas.add(bicicleta); // TODO
-    } else {
+    if (response.statusCode != 201) {
       throw Exception('Fallo al agregar la bici: ${response.body}');
     }
-
   }
 
+  // Solicitud PUT
+  Future<void> updateBicicleta(int id, Map<String, dynamic> bicicleta) async {
+    final response = await http.put(
+      Uri.parse('$apiUrl/$id'),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode(bicicleta),
+    );
+    if (response.statusCode != 200) {
+      throw Exception('Error actualizando bicicleta');
+    }
+  }
+
+  // Solicitud DELETE
+
+  // Solicitud GET
 }
