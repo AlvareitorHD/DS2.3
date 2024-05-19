3c5b49a89692c96b0c0da4370d89aa4833760828
lu1smgb <lu1smgb.gacc@gmail.com>
2024-05-19 13:53:38 +0000

    P4: Operacion create provisional

--- P2/api/app/controllers/bicicletas_controller.rb
+++ P2/api/app/controllers/bicicletas_controller.rb
@@ -1,15 +1,13 @@
 class BicicletasController < ApplicationController
 
     def create
-        # Dilema:
-        # Transferir patrones de disenio al backend??? (app/models)
-        @tipo = bicicleta_params[:tipo_bicicleta]
+        @tipo = params[:tipo_bicicleta]
         @bicicleta = nil
         case @tipo.downcase
         when 'montana'
-            @bicicleta = BicicletaMontana.new(bicicleta_montana_params)
+            @bicicleta = Bicicleta.new(bicicleta_montana_params)
         when 'carretera'
-            @bicicleta = BicicletaCarretera.new(bicicleta_carretera_params)
+            @bicicleta = Bicicleta.new(bicicleta_carretera_params)
         end
 
         if @bicicleta != nil && @bicicleta.save
@@ -45,16 +43,6 @@ class BicicletasController < ApplicationController
     end
 
     def bicicleta_montana_params
-        # params.require(:bicicleta).permit(:tipo_manillar, 
-        #                                   :tipo_frenos,
-        #                                   :num_frenos,
-        #                                   :tipo_transmision, 
-        #                                   :tipo_cuadro, 
-        #                                   :tipo_sillin,
-        #                                   :tipo_ruedas,
-        #                                   :num_ruedas,
-        #                                   :imagen_representativa,
-        #                                   :tipo_bicicleta)
         bicicleta_carretera_params.permit(:tipo_suspension,
                                           :num_suspensiones)
     end
